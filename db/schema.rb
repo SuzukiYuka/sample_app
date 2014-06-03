e1660d0cfb5ef5a76b5f7b90c774024fd385e7f2
SUZUKIyuka <suzukiyuka1218@gmail.com>
2014-06-03 18:28:14 +0900

    error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140531021341) do
+ActiveRecord::Schema.define(version: 20140602104928) do
 
   create_table "users", force: true do |t|
     t.string   "name"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
 
   	before do
   		@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
-	 end
+    end
 
   subject { @user }
 
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -95,6 +96,11 @@ describe User do
     end
   end
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+  
 end
    
 
