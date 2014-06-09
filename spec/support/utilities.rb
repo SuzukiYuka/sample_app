c039ccba27bf6897f22e6f4c63366f4b04a42493
SUZUKIyuka <suzukiyuka1218@gmail.com>
2014-06-09 21:30:57 +0900

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -4,6 +4,7 @@ def valid_signin(user)
 	fill_in "Email", with: user.Email
 	fill_in "Password", with: user.password
 	click_button "Sing in"
+end
 
 RSpec::Matchers.define :have_error_message do |message|
 	match do |page|
@@ -12,12 +13,25 @@ RSpec::Matchers.define :have_error_message do |message|
 end
 
 
-
 def full_title(page_title)
 	base_title = "Ruby on Rails Tutorial Sample App"
 	if page_title.empty?
 		base_title
 	else
 		"#{base_title} | #{page_title}"
+	end
+end
+
+def sign_in(user, options={})
+	if options[:no_capybara]
+		#Capybaraを使用していない場合にもサインインする。
+		remember_token = User.new_remember_token
+		cookies[:remember_token] = remember_token
+		user.update_attribute(:remember_token, User.encrypt(remember_token))
+	else
+		visit signin_path
+		fill_in "Email", with: user.email
+		fill_in "Password", with: user.password
+		click_button "Sign in"
 	end
 end
