8862d07d655c0bfd3214aa1bbfa52df66d8586cd
SUZUKIyuka <suzukiyuka1218@gmail.com>
2014-07-01 19:45:20 +0900

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "User　Pages" do
+describe "User pages" do
 	subject { page }
 
 	describe "index" do
@@ -51,13 +51,30 @@ describe "User　Pages" do
 
 	describe "profile page" do
 		let(:user){ FactoryGirl.create(:user) }
+		let!(:m1){ FactoryGirl.create(:micropost, user: user, content: "Foo") }
+		let!(:m2){ FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
 		before { visit user_path(user) }
 
 		it { should have_content(user.name) }
 		it { should have_title(user.name) }
+
+		describe "microposts" do
+			it { should have_content(m1.content) }
+			it { should have_content(m2.content) }
+			it { should have_content(user.microposts.count) }
+		end
+	end
+
+	describe "signup page" do
+		before { visit signup_path }
+
+		it { should have_content('Sign up') }
+		it { should have_title(full_title('Sign up')) }
 	end
 
 	describe "signup" do
+
 		before { visit signup_path }
 
 		let(:submit){"Create my account"}
@@ -66,6 +83,13 @@ describe "User　Pages" do
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User,:count)
 			end
+
+			describe "after submission" do
+				before { click_button submit }
+
+				it { should have_title('Sign up') }
+				it { should have_content('error') }
+			end
 		end
 
 		describe "with valid information" do
@@ -73,12 +97,13 @@ describe "User　Pages" do
 				fill_in "Name", with:"Example User"
 				fill_in "Email", with:"user@example.com"
 				fill_in "Password", with: "foobar"
-				fill_in "Confirmation", with: "foobar"
+				fill_in "Confirm Password", with: "foobar"
 			end
 
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end
+
 
 			describe "after saving the user" do
 				before { click_button submit }
@@ -90,12 +115,13 @@ describe "User　Pages" do
 			end
 		end  
 	end	
+
 	describe "edit" do
-	let(:user) { FactoryGirl.create(:user) }
-	before do 
-		sign_in user
-		visit edit_user_path(user) 
-	end
+		let(:user) { FactoryGirl.create(:user) }
+		before do 
+			sign_in user
+			visit edit_user_path(user) 
+		end
 
 		describe "page" do
 			it { should have_content("Update your profile") }
@@ -125,6 +151,17 @@ describe "User　Pages" do
 			it { should have_link('Sign out', href: signout_path) }
 			specify { expect(user.reload.name).to eq new_name }
 			specify { expect(user.reload.email).to eq new_email }
+		end
+
+		describe "forbidden attributes" do
+			let(:params) do
+				{ user: { admin: true, password: user.password, password_confirmation: user.password } }
+			end
+			before do
+				sign_in user, no_capybara: true
+				patch user_path(user), params
+			end
+			specify { expect(user.reload).not_to be_admin }
 		end
 	end
 end
